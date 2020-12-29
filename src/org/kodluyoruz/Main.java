1df7b902c18840cf81365f1224cc443d61023e11
MerveAltunay <76403911+MerveAltunay@users.noreply.github.com>
2020-12-29 23:41:54 +0300

    SosGame

--- src/org/kodluyoruz/Main.java
+++ src/org/kodluyoruz/Main.java
@@ -50,6 +50,7 @@ public class Main {
                 playerLetter="o";
                 computerLetter="s";
             }
+            //System.out.println();
 
 
             if (startPerson == 0) {
